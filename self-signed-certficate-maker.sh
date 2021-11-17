58c64862fb40871d6bcc9fccee544f9fb8dd534e
flyingbarbarian <50474981+flyingbarbarian@users.noreply.github.com>
2021-11-17 19:09:21 +0900

    #3 証明書発行機能の実装
    証明書発行機能の実装

--- self-signed-certficate-maker.sh
+++ self-signed-certficate-maker.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+####################
+# 設定の読み込みなど
+####################
 # 設定項目の読み込み
 source ./settings/setting.sh
 
 # 出力先のディレクトリを作成する
 mkdir $RESULTS
 
+
+####################
+# 証明書の発行など
+####################
 # 秘密鍵の生成
 openssl genrsa -out $PRIVATE_KEY_NAME $PRIVATE_KEY_NUMBITS
 
@@ -17,5 +24,24 @@ openssl req -new -key $PRIVATE_KEY_NAME -out $CERTIFICATE_SIGNING_REQUEST_NAME -
 
 # オレオレ証明書の発行
 openssl x509 -req -in $CERTIFICATE_SIGNING_REQUEST_NAME -signkey $PRIVATE_KEY_NAME -out $CERTIFICATE_NAME -days $EXPIRATION
+
+
+####################
+# 履歴の作成など
+####################
+# 履歴を使わない場合はここで終了
+if [ $HISROY_USE -eq 0 ]
+then
+    exit 0
+fi
+
+# history.csvが存在しない場合は、ファイルを作成してヘッダ行を追加する。
+if [ ! -f $HISTORY ]
+then
+    echo "発行時刻, 公開鍵暗号方式, 秘密鍵の長さ, 秘密鍵の暗号化, 証明書有効期限, 秘密鍵名, 公開鍵名, CSR名, 証明書名" > $HISTORY
+fi
+
+# 履歴の作成
+echo "${TODAY}, ${PUBLIC_KEY_CRYPTOSYSTEM}, ${PRIVATE_KEY_NUMBITS}, ${PRIVATE_KEY_ENCRYPTION}, ${EXPIRATION}, ${PRIVATE_KEY_NAME}, ${PUBLIC_KEY_NAME}, ${CERTIFICATE_SIGNING_REQUEST_NAME}, ${CERTIFICATE_NAME}" >> $HISTORY
 
 exit 0
